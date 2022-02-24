2220e4ab04ca18318fb9f0edd751d3af9689b2da
Jean Paull <t3009nln@prestadorcbmp.com.br>
2022-02-24 20:02:48 -0300

    add chart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,11 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:financeapp/components/rounded_button.dart';
 import 'package:financeapp/utils/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+
+import '../components/bar_chart.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,19 +16,36 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Finances App'),
         automaticallyImplyLeading: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: InkWell(
+              child: const Icon(Icons.logout),
+              onTap: () {
+                AwesomeDialog(
+                  context: context,
+                  dialogType: DialogType.QUESTION,
+                  headerAnimationLoop: false,
+                  animType: AnimType.SCALE,
+                  title: FirebaseAuth.instance.currentUser?.displayName ??
+                      "Logout",
+                  desc: 'Deseja realmente sair do app...',
+                  buttonsTextStyle: const TextStyle(color: Colors.black),
+                  showCloseIcon: false,
+                  btnCancelOnPress: () {},
+                  btnOkOnPress: () async {
+                    await FirebaseAuth.instance
+                        .signOut()
+                        .whenComplete(() => Navigator.pop(context));
+                  },
+                ).show();
+              },
+            ),
+          ),
+        ],
       ),
       backgroundColor: Colors.white,
-      body: Center(
-        child: ButtonRounded(
-          label: "LogOut",
-          buttonBackgroundColor: primaryColor,
-          width: 150,
-          callback: () {
-            FirebaseAuth.instance.signOut();
-            Navigator.pop(context);
-          },
-        ),
-      ),
+      body: BarChartPage(),
     );
   }
 }
