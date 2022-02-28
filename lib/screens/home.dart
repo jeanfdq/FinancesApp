d1d2433ee714a04b05eca57b1a98eea2f3f3c3af
Jean Paull <t3009nln@prestadorcbmp.com.br>
2022-02-28 17:01:03 -0300

    add list of finances

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,11 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:financeapp/components/pie_chart2.dart';
 import 'package:financeapp/utils/extensions/ext_card.dart';
+import 'package:financeapp/utils/extensions/ext_color.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../components/bar_chart.dart';
 import '../components/pie_chart.dart';
 
@@ -18,11 +20,18 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Finances App'),
         automaticallyImplyLeading: false,
+        leading: InkWell(
+          child: const Icon(
+            Icons.person,
+            color: Colors.white,
+          ),
+          onTap: (){},
+        ),
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 16),
             child: InkWell(
-              child: const Icon(Icons.logout),
+              child: const Icon(Icons.add_circle),
               onTap: () {
                 AwesomeDialog(
                   context: context,
@@ -47,32 +56,43 @@ class Home extends StatelessWidget {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            pieChart2(context, 300, screenSize.width),
-            RoundedCard.get(
-              context,
-              60,
-              backgroundColor: Colors.white,
-              child: const Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 12),
-                  child: Text(
-                    "Seu Gastos...",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          pieChart2(context, 300, screenSize.width),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: const Icon(
+                    Icons.monetization_on_outlined,
+                    color: Colors.red,
                   ),
-                ),
-              ),
+                  title: const Text("Title"),
+                  subtitle: const Text("SubTitle"),
+                  trailing: Container(
+                    width: 95,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Color.alphaBlend(
+                          HexColor.fromHex("ffcccb"), Colors.red),
+                      borderRadius: const BorderRadius.all(Radius.circular(18)),
+                    ),
+                    child: const Center(
+                        child: Text(
+                      "R\$ 1000,00",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    )),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
